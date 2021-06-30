4ee96d847136339dfd80be3fe74cadac6cf93507
Ingrid-Paulino <ingridpaulino1012@gmail.com>
2021-06-30 17:29:18 -0300

    C7 - Adicionado requisito 9

--- src/challenges.js
+++ src/challenges.js
@@ -30,15 +30,18 @@ function footballPoints(wins, ties) {
 
 // Desafio 6
 function highestCount(numeros) {
-  let nRepete = 0;
-  for(let index = 0; index < numeros.length; index += 1) {
-    if(numeros[index] === numeros[index]) {
-      nRepete += numeros[index]
-      return nRepete
+  for(let index = 0; index < numeros.length; index += 1) {  let nRepete = 1;
+    let qtdRepete = 0;
+    for(let indexIgual = 0; indexIgual < numeros.length; indexIgual += 1) {
+      if(numeros[index] == numeros[indexIgual]) {
+      nRepete += 1
+      qtdRepete = numeros[index];
+      }
     }
+    return qtdRepete
   }
 }
-console.log(highestCount([9, 1, 2, 3, 5, 3, 7]));
+console.log(highestCount([10, 1, 10, 3, 5, 3, 7]));
 
 
 //Desafio 7
@@ -49,20 +52,25 @@ function catAndMouse(mouse, cat1, cat2 ) {
     return 'cat1';
   }else if(calculoCat1 < calculoCat2){
     return 'cat2';
-  }else {
+  } else {
     return "Os gatos trombam e o rato foge";
   }
+  
+ 
 }
-  console.log(catAndMouse(8, 6, 12));
+  console.log(catAndMouse(1, 0, 2));
 
 // Desafio 8
 function fizzBuzz(numerosArray) {
+  
   for(let index = 0; index < numerosArray.length; index += 1) {
-    if((numerosArray[index]/3) % 0) {
+    let res = '';
+    if(numerosArray[index]%3 == 0) {
       return "fizz";
-    }else if((numerosArray[index]/5) % 0 ){
+      res.push([index])
+    }else if(numerosArray[index]%5 == 0 ){
       return "buzz";
-    }else if((numerosArray[index]/3) % 0 && (numerosArray[index]/5) % 0 ) {
+    }else if(numerosArray[index]%3 == 0 && numerosArray[index]%5 == 0 ) {
       return "fizzBuzz";
     }else {
       return "bug!";
@@ -73,25 +81,41 @@ console.log(fizzBuzz([2, 15, 7, 9, 45]));
 
 // Desafio 9
   function encode(letrasParaNumeros) {
-    let frase = '';
-    frase = letrasParaNumeros.replace(/a/g, '1');
-    frase = letrasParaNumeros.replace(/e/g, '2');
-    frase = letrasParaNumeros.replace(/i/g, '3');
-    frase = letrasParaNumeros.replace(/o/g, '4');
-    frase = letrasParaNumeros.replace(/u/g, '5');
-    return frase
-    }
+    for(let index = 0; index < letrasParaNumeros.length; index +=1) {
+      if(letrasParaNumeros[index] == 'a'){
+        letrasParaNumeros = letrasParaNumeros.replace('a', '1')
+      }else if(letrasParaNumeros[index] == 'e'){
+        letrasParaNumeros = letrasParaNumeros.replace('e', '2')
+      }else if(letrasParaNumeros[index] == 'i'){
+        letrasParaNumeros = letrasParaNumeros.replace('i', '3')
+      }else if(letrasParaNumeros[index] == 'o'){
+        letrasParaNumeros = letrasParaNumeros.replace('o', '4')
+      }else if(letrasParaNumeros[index] == 'u'){
+        letrasParaNumeros = letrasParaNumeros.replace('u', '5')
+      }
+  }
+  return letrasParaNumeros
+}
+
 console.log(encode('hi there!'));
 
 function decode(numeroParaLetra) {
-  let frase2 = '';
-  frase2 = numeroParaLetra.replace(/1/g, 'a');
-  frase2 = numeroParaLetra.replace(/2/g, 'e');
-  frase2 = numeroParaLetra.replace(/3/g, 'i');
-  frase2 = numeroParaLetra.replace(/4/g, 'o');
-  frase2 = numeroParaLetra.replace(/3/g, 'u');
-  return frase2
+  for(let index = 0; index < numeroParaLetra.length; index +=1) {
+    if(numeroParaLetra[index] == '1'){
+      numeroParaLetra = numeroParaLetra.replace('1', 'a')
+    }else if(numeroParaLetra[index] == '2'){
+      numeroParaLetra = numeroParaLetra.replace('2', 'e')
+    }else if(numeroParaLetra[index] == '3'){
+      numeroParaLetra = numeroParaLetra.replace('3', 'i')
+    }else if(numeroParaLetra[index] == '4'){
+      numeroParaLetra = numeroParaLetra.replace('4', 'o')
+    }else if(numeroParaLetra[index] == '5'){
+      numeroParaLetra = numeroParaLetra.replace('5', 'u')
+    }
 }
+  return numeroParaLetra
+}
+
 console.log(decode('h3 th2r2!'));
 
 
